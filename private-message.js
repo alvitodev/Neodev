e28cf7be8f2b1e4a5f966b9bf0393bb0ad55a3bd
Alvito Devanova <zillioncraft701@gmail.com>
2021-03-10 16:42:56 +0700

    added private message

--- private-message.js
+++ private-message.js
@@ -1,10 +1,10 @@
 module.exports = (client, triggerText, replyText) => {
     client.on('message', (message) => {
-        if (
-            message.channel.type === 'dm' &&
-            message.content.toLowerCase() === triggerText.toLowerCase()
-        ) {
-            message.author.send(replyText)
-        }
+      if (
+        message.channel.type === 'dm' &&
+        message.content.toLowerCase() === triggerText.toLowerCase()
+      ) {
+        message.author.send(replyText)
+      }
     })
-}
+  }
